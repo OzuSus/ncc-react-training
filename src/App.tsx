b8655c90a4b7fb768146ca1c978078c8f578a586
bach.tranxuan <bach.tranxuan@ncc.asia>
2025-04-21 17:30:19 +0700

    fix testcase failed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo512.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
