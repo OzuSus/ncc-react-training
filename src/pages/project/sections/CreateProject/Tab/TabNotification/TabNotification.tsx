6d80e2829838052658b12285b83bb6dba3193966
duoc.phungvan <duoc.phungvan@ncc.asia>
2026-05-28 17:39:09 +0700

    feat: create tab notification for create project

---
+++ src/pages/project/sections/CreateProject/Tab/TabNotification/TabNotification.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Box, Checkbox, FormControlLabel } from '@mui/material';
+import { Controller, useFormContext } from 'react-hook-form';
+import { CustomTypography } from '@/libs/components/ui/Typography';
+import { ICreateProjectForm } from '@/pages/project/sections/CreateProject';
+import { CustomTextField } from '@/libs/components/ui/TextField';
+import { NOTIFICATION_OPTIONS } from '@/libs/constants/notification.ts';
+
+export default function TabNotification() {
+  const { control } = useFormContext<ICreateProjectForm>();
+
+  return (
+    <Box
+      sx={{ px: 3, py: 2, display: 'flex', flexDirection: 'column', gap: 1.5 }}
+    >
+      <Controller
+        name="komuChannelId"
+        control={control}
+        render={({ field }) => (
+          <CustomTextField {...field} placeholder="Komu Channel Id" fullWidth />
+        )}
+      />
+      {NOTIFICATION_OPTIONS.map(({ label, field }) => (
+        <Controller
+          name={field}
+          control={control}
+          render={({ field: field }) => (
+            <FormControlLabel
+              sx={{ m: 0 }}
+              control={
+                <Checkbox
+                  checked={!!field.value}
+                  onChange={field.onChange}
+                  size="medium"
+                  sx={{
+                    mr: 0.5,
+                    color: '#c5c4c4',
+                    '&.Mui-checked': { color: '#4680ff' },
+                  }}
+                />
+              }
+              label={
+                <CustomTypography
+                  sx={{ fontSize: 14, fontWeight: 500, color: '#1D2630' }}
+                >
+                  {label}
+                </CustomTypography>
+              }
+            />
+          )}
+        />
+      ))}
+    </Box>
+  );
+}
