8bc0d6caa8164ddd9ef824b807c3e1cbaf9924dd
duoc.phungvan <duoc.phungvan@ncc.asia>
2026-06-05 14:25:02 +0700

    feat: setup i18n and create dropdown select language

---
+++ src/libs/constants/languages.ts
@@ -0,0 +1,43 @@
+export const LANGUAGES = [
+  { code: 'en', label: 'English', flag: 'https://flagsapi.com/US/flat/24.png' },
+  {
+    code: 'vi',
+    label: 'Tiếng Việt',
+    flag: 'https://flagsapi.com/VN/flat/24.png',
+  },
+  {
+    code: 'es',
+    label: 'Español (México)',
+    flag: 'https://flagsapi.com/MX/flat/24.png',
+  },
+  {
+    code: 'fr',
+    label: 'Français',
+    flag: 'https://flagsapi.com/FR/flat/24.png',
+  },
+  { code: 'de', label: 'Deutsch', flag: 'https://flagsapi.com/DE/flat/24.png' },
+  {
+    code: 'it',
+    label: 'Italiano',
+    flag: 'https://flagsapi.com/IT/flat/24.png',
+  },
+  {
+    code: 'nl',
+    label: 'Nederlands',
+    flag: 'https://flagsapi.com/NL/flat/24.png',
+  },
+  {
+    code: 'pt',
+    label: 'Português',
+    flag: 'https://flagsapi.com/PT/flat/24.png',
+  },
+  { code: 'tr', label: 'Türkçe', flag: 'https://flagsapi.com/TR/flat/24.png' },
+  { code: 'ru', label: 'Русский', flag: 'https://flagsapi.com/RU/flat/24.png' },
+  { code: 'ar', label: 'العربية', flag: 'https://flagsapi.com/SA/flat/24.png' },
+  { code: 'ja', label: '日本語', flag: 'https://flagsapi.com/JP/flat/24.png' },
+  {
+    code: 'zh',
+    label: '简体中文',
+    flag: 'https://flagsapi.com/CN/flat/24.png',
+  },
+];
